29ce09ce61ab76ea18f511139d14135c4768ab66
skathuriya07 <57830101+skathuriya07@users.noreply.github.com>
2021-07-30 00:18:10 +0530

    Add files via upload

---
+++ ShreyaKathuriya-A3-iOsTests/ShreyaKathuriya_A3_iOsTests.swift
@@ -0,0 +1,33 @@
+//
+//  ShreyaKathuriya_A3_iOsTests.swift
+//  ShreyaKathuriya-A3-iOsTests
+//
+//  Created by Shreya Kathuriya on 02/05/21.
+//
+
+import XCTest
+@testable import ShreyaKathuriya_A3_iOs
+
+class ShreyaKathuriya_A3_iOsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
